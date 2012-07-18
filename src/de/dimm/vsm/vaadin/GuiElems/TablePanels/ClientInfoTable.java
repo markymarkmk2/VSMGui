c39a20334aa750adea265002b96b2062f1ce703c
Mark Williams <mark@dimm.de>
2012-07-18 11:37:36 +0000

    Enc + Comp

--- src/de/dimm/vsm/vaadin/GuiElems/TablePanels/ClientInfoTable.java
+++ src/de/dimm/vsm/vaadin/GuiElems/TablePanels/ClientInfoTable.java
@@ -5,9 +5,13 @@
 
 package de.dimm.vsm.vaadin.GuiElems.TablePanels;
 
+import com.vaadin.data.Validator.InvalidValueException;
+import com.vaadin.data.validator.AbstractValidator;
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Window;
 import de.dimm.vsm.fsengine.GenericEntityManager;
+import de.dimm.vsm.net.interfaces.AgentApi;
 import de.dimm.vsm.records.ClientInfo;
 import de.dimm.vsm.records.ClientVolume;
 import de.dimm.vsm.records.Excludes;
@@ -21,7 +25,47 @@ import de.dimm.vsm.vaadin.VSMCMain;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+class AgentOptValidator extends AbstractValidator
+{
+    String key;
+    String val;
+    ClientInfoTable clit;
+    
+
+    public AgentOptValidator( ClientInfoTable clit, String key, String val )
+    {
+        super(VSMCMain.Txt("Dieser Agent unterstützt dieses Feature nicht"));
+        this.key = key;
+        this.val = val;
+        this.clit = clit;
+    }
+
+   
+
+    @Override
+    public boolean isValid( Object value )
+    {
+        if (value instanceof Boolean)
+        {
+            Boolean b = (Boolean) value;
+            if (b.booleanValue())
+            {
+                Properties p = getProperties();
+                if (p != null)
+                    return  (p.getProperty(key, "sdhaks").equals(val));
+            }
+        }
+        return true;
+    }
+    
+    Properties getProperties()
+    {
+        return clit.getAgentProperties();
+    }
+
+}
 /**
  *
  * @author Administrator
@@ -38,24 +82,35 @@ public class ClientInfoTable extends BaseDataEditTable<ClientInfo>
 
     public static ClientInfoTable createTable( VSMCMain main, Schedule sched, List<ClientInfo> list, ItemClickListener listener)
     {
-        GenericEntityManager em = main.get_util_em(sched.getPool());
+        GenericEntityManager em = VSMCMain.get_util_em(sched.getPool());
 
         ArrayList<JPAField> fieldList = new ArrayList<JPAField>();
         fieldList.add(new JPATextField(VSMCMain.Txt("IP"), "ip"));
         fieldList.add(new JPATextField(VSMCMain.Txt("Port"), "port"));
         fieldList.add(new JPACheckBox(VSMCMain.Txt("Gesperrt"), "disabled"));
-        fieldList.add(new JPACheckBox(VSMCMain.Txt("Compression"), "compression"));
-        fieldList.add(new JPACheckBox(VSMCMain.Txt("NurNeuereDateien"), "onlyNewer"));
+        fieldList.add(new JPACheckBox(VSMCMain.Txt("Komprimierung"), "compression"));
+        fieldList.add(new JPACheckBox(VSMCMain.Txt("Verschlüsselung"), "encryption"));
+        fieldList.add(new JPACheckBox(VSMCMain.Txt("Nur neue Dateien"), "onlyNewer"));
         fieldList.add(new JPADBLinkField( em, VSMCMain.Txt("Volumes"), "volumeList", ClientVolume.class));
         fieldList.add(new JPADBLinkField( em, VSMCMain.Txt("Excludes"), "exclList", Excludes.class));
+
+        setTableColumnVisible(fieldList, "compression", false);
+        setTableColumnVisible(fieldList, "encryption", false);
+        setTableColumnVisible(fieldList, "onlyNewer", false);
+
         
-        return new ClientInfoTable( main, sched, list, fieldList, listener);
+        ClientInfoTable clit = new ClientInfoTable( main, sched, list, fieldList, listener);
+
+        setFieldValidator(fieldList, "compression", new AgentOptValidator(clit, AgentApi.OP_AG_COMP, "true"));
+        setFieldValidator(fieldList, "encryption", new AgentOptValidator(clit, AgentApi.OP_AG_ENC, "true"));
+
+        return clit;
     }
 
     @Override
     protected GenericEntityManager get_em()
     {
-        return main.get_util_em(sched.getPool());
+        return VSMCMain.get_util_em(sched.getPool());
     }
 
 
@@ -100,11 +155,6 @@ public class ClientInfoTable extends BaseDataEditTable<ClientInfo>
 
         return null;
     }
-    @Override
-    protected String getTablenameText()
-    {
-        return VSMCMain.Txt(this.getClass().getSimpleName());
-    }
     
     @Override
     protected void setDBWinLayout( Window win )
@@ -128,5 +178,37 @@ public class ClientInfoTable extends BaseDataEditTable<ClientInfo>
         return true;
     }
 
+    String lastIP = null;
+    int lastPort = -1;
+    Properties props;
+    Properties getAgentProperties()
+    {
+        if (activeElem == null)
+            return null;
+
+        if (activeElem.getIp() == null)
+            return null;
+
+        if (activeElem.getIp().isEmpty())
+            return null;
+
+        if (lastIP == null || lastPort != getActiveElem().getPort() || !lastIP.equals(getActiveElem().getIp()))
+        {
+            props = main.getGuiServerApi().getAgentProperties( activeElem.getIp(), activeElem.getPort(), /*withMsg*/ false );
+            if (props == null)
+                VSMCMain.notify(this, VSMCMain.Txt("Dieser Agent ist nicht online"), "Optionen können nicht abgefragt werden");
+            lastIP = activeElem.getIp();
+            lastPort = activeElem.getPort();
+        }
+        return props;
+    }
+
+    @Override
+    public AbstractOrderedLayout createEditComponentPanel( boolean readOnly )
+    {
+        props = null;
+        lastIP = null;
+        return super.createEditComponentPanel(readOnly);
+    }
 
 }
