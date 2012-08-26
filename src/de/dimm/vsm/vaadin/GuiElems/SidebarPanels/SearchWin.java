7ad2c8b6569d097fd55c2daa4aff28ef3fa9fabf
Mark Williams <mark@dimm.de>
2012-08-26 20:41:03 +0000

    Added User

--- src/de/dimm/vsm/vaadin/GuiElems/SidebarPanels/SearchWin.java
+++ src/de/dimm/vsm/vaadin/GuiElems/SidebarPanels/SearchWin.java
@@ -512,7 +512,7 @@ public class SearchWin extends SidebarPanel
 
         };
 
-        container = new FSTreeContainer(provider, fields);
+        container = new FSTreeContainer(provider, fields, main.getUser());
         if (root_list != null)
             container.initRootlist(root_list);
 
