5d9811a3bef207df6093286d2c6ccadeee3c99d5
Mark Williams <mark@dimm.de>
2012-08-26 20:43:02 +0000

    Added usermapping, Statistic

---
+++ src/de/dimm/vsm/vaadin/GuiElems/Dialogs/BlockStatusDlg.java
@@ -0,0 +1,241 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.dimm.vsm.vaadin.GuiElems.Dialogs;
+
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.NativeButton;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import de.dimm.vsm.Utilities.SizeStr;
+import de.dimm.vsm.net.PoolStatusResult;
+import de.dimm.vsm.records.StoragePool;
+import de.dimm.vsm.vaadin.VSMCMain;
+
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class BlockStatusDlg extends Window
+{
+
+     StoragePool pool;
+     VSMCMain main;
+     
+     VerticalLayout vl = new VerticalLayout();
+    
+
+    TextField ddTotalBlocks;
+    TextField dedupDataLen;
+    TextField actFsDataLen;
+    TextField actFileCnt;
+    TextField totalFsDataLen;
+    TextField totalFileCnt;
+    TextField removeDDCnt;
+    TextField removeDDLen;
+    TextField dedupRatio;
+    TextField totalDedupRatio;
+
+
+    public BlockStatusDlg( VSMCMain main, StoragePool pool )
+    {
+        this.main = main;
+        this.pool = pool;
+
+        build_gui();
+
+        updateContent();
+    }
+    
+
+    final void build_gui( )
+    {
+        addComponent(vl);
+
+
+        setModal(true);
+        setStyleName("vsm");
+
+        //this.setSizeFull();
+        this.setWidth("400px");
+
+        vl.setSpacing(true);
+        vl.setSizeFull();
+        vl.setImmediate(true);
+        vl.setStyleName("editWin");
+
+        this.setCaption(VSMCMain.Txt("Block_Informationen_für_StoragePool") + " " + pool.getName());
+
+        ddTotalBlocks = new TextField(VSMCMain.Txt("Anzahl Blöcke insg."));
+        vl.addComponent(ddTotalBlocks);
+
+        dedupDataLen = new TextField(VSMCMain.Txt("Speicherplatz Blöcke"));
+        vl.addComponent(dedupDataLen);
+
+        actFsDataLen = new TextField(VSMCMain.Txt("Aktuelle Größe Dateisystem"));
+        vl.addComponent(actFsDataLen);
+
+        actFileCnt = new TextField(VSMCMain.Txt("Aktuelle Anzahl Dateien/Verzeichnise"));
+        vl.addComponent(actFileCnt);
+
+        totalFileCnt = new TextField(VSMCMain.Txt("Anzahl Dateien/Verzeichnise inkl. Historie"));
+        vl.addComponent(totalFileCnt);
+
+        totalFsDataLen = new TextField(VSMCMain.Txt("Größe Dateisystem inkl. Historie"));
+        vl.addComponent(totalFsDataLen);
+
+        removeDDCnt = new TextField(VSMCMain.Txt("Freie Blöcke insg."));
+        vl.addComponent(removeDDCnt);
+
+        removeDDLen = new TextField(VSMCMain.Txt("Speicherplatz freie Blöcke"));
+        vl.addComponent(removeDDLen);
+
+        dedupRatio = new TextField(VSMCMain.Txt("DedupRatio aktuelles Dateisystem."));
+        vl.addComponent(dedupRatio);
+
+        totalDedupRatio = new TextField(VSMCMain.Txt("DedupRatio Dateisystem inkl. Historie"));
+        vl.addComponent(totalDedupRatio);
+
+
+        Button calc = new NativeButton(VSMCMain.Txt("Berechnen (dauert einige Zeit)"));
+        vl.addComponent(calc);
+
+        Button removeBlocks = new NativeButton(VSMCMain.Txt("Freie Blöcke entfernen"));
+        vl.addComponent(removeBlocks);
+
+        Button close = new NativeButton(VSMCMain.Txt("Ok"));
+
+        vl.addComponent(close);
+        vl.setComponentAlignment(close, Alignment.BOTTOM_RIGHT);
+
+        final Window w = this;
+        close.addListener( new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                event.getButton().getApplication().getMainWindow().removeWindow(w);
+            }
+        });
+
+        calc.addListener( new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                calcStatus();
+            }
+        });
+        removeBlocks.addListener( new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                removeBlocks();
+            }
+        });
+    }
+
+    private void updateContent()
+    {
+        PoolStatusResult psr = (PoolStatusResult)VSMCMain.callLogicControl("getPoolStatusResult", pool);
+        if (psr == null)
+        {
+            VSMCMain.notify(vl, "Fehler beim Abholen der Statusergebnisse", pool.getName());
+            return;
+        }
+        
+        setResult( ddTotalBlocks, psr.getDdTotalBlocks() );
+        setResult( dedupDataLen, psr.getDedupDataLen() );
+        setResult( actFsDataLen, psr.getActFsDataLen() );
+        setResult( actFileCnt, psr.getActFileCnt() );
+        setResult( totalFsDataLen, psr.getTotalFsDataLen() );
+        setResult( totalFileCnt, psr.getTotalFileCnt() );
+        setResult( removeDDCnt, psr.getRemoveDDCnt() );
+        setResult( removeDDLen, psr.getRemoveDDLen() );
+        setResult( dedupRatio, psr.getDedupRatio(), true );
+        setResult( totalDedupRatio, psr.getTotalDedupRatio(), true );
+
+    }
+
+    void calcStatus()
+    {
+
+       main.runInBusyCancel("Berechnung läuft", new Runnable() {
+
+            @Override
+            public void run()
+            {
+                VSMCMain.callLogicControl("calcPoolStatusResult", pool);
+                updateContent();
+            }
+        }, new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                VSMCMain.callLogicControl("abortCalcPoolStatusResult", pool);
+            }
+        } );
+
+    }
+
+    void removeBlocks()
+    {
+
+       main.runInBusyCancel("Bereinigen läuft", new Runnable() {
+
+            @Override
+            public void run()
+            {
+                Object o = VSMCMain.callLogicControl("deleteFreeBlocks", pool);
+                if (o != null && o instanceof Long)
+                {
+                    Long l = (Long)o;
+                    main.Msg().info("Es wurden insgesamt " + l.longValue() + " Blöcke entfernt" , null);
+
+                    if (l.longValue() > 0)
+                    {
+                        calcStatus();
+                    }
+                }
+            }
+        }, new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                VSMCMain.callLogicControl("abortDeleteFreeBlocks");
+            }
+        } );
+    }
+
+   
+
+
+    private void setResult( TextField tf, long ddTotalBlocks0 )
+    {
+        setResult(tf, ddTotalBlocks0, false);
+    }
+    private void setResult( TextField tf, long ddTotalBlocks0, boolean percent )
+    {
+        String s = "-";
+        if (ddTotalBlocks0 > 0)
+        {
+            if (percent)
+                s = Long.toString(ddTotalBlocks0) + "%";
+            else
+                s = SizeStr.format(ddTotalBlocks0 ) + " (" + Long.toString(ddTotalBlocks0) + ")";
+        }
+        tf.setValue( s );        
+        tf.setWidth("100%");
+    }
+
+}
---
+++ src/de/dimm/vsm/vaadin/GuiElems/Dialogs/VsmFsMappingDlg.java
@@ -0,0 +1,230 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.dimm.vsm.vaadin.GuiElems.Dialogs;
+
+import com.vaadin.data.Validator;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeButton;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import de.dimm.vsm.net.StoragePoolWrapper;
+import de.dimm.vsm.net.interfaces.GuiServerApi;
+import de.dimm.vsm.vaadin.GuiElems.Fields.JPADBFSField;
+import de.dimm.vsm.vaadin.GuiElems.Fields.JPAPoolComboField;
+import de.dimm.vsm.vaadin.GuiElems.Fields.JPARemoteFSField;
+import de.dimm.vsm.vaadin.GuiElems.FileSystem.DBFSElemEditor;
+import de.dimm.vsm.vaadin.GuiElems.FileSystem.FSTree;
+import de.dimm.vsm.vaadin.GuiElems.FileSystem.RemoteFSElemTreeElem;
+import de.dimm.vsm.vaadin.GuiElems.OkAbortPanel;
+import de.dimm.vsm.vaadin.VSMCMain;
+
+/**
+ *
+ * @author Administrator
+ */
+public class VsmFsMappingDlg extends TextAreaDlg
+{
+    TextField tf;
+    VSMCMain main;
+    
+    
+    public VsmFsMappingDlg( String caption, String def, Validator val, VSMCMain main)
+    {
+        super( caption, def, val, true, true );
+        this.main = main;
+    }
+   
+
+    @Override
+    final void build_gui(  String caption, String def, Validator val, boolean hasAbort, boolean rdOnly  )
+    {
+        VerticalLayout main = new VerticalLayout();
+        setContent(main);
+        main.setSpacing(true);
+        main.setSizeFull();
+        main.setStyleName("editWin");
+        main.setImmediate(true);
+
+        validator = val;
+
+        setModal(true);
+        setStyleName("vsm");
+
+        //this.setSizeFull();
+        this.setWidth("500px");
+        this.setHeight("600px");
+
+        this.setCaption(caption);
+
+        HorizontalLayout hl = new HorizontalLayout();
+        hl.setSizeFull();
+        hl.setSpacing(true);
+
+
+      
+        tfText = new TextArea("", def);
+        tfText.setReadOnly(rdOnly);
+        if (val != null)
+            ((TextArea)tfText).addValidator( val );
+
+        tfText.setSizeFull();
+        hl.addComponent(tfText);
+        hl.setExpandRatio(tfText, 1.0f);
+
+
+        main.addComponent(hl);
+        main.setExpandRatio(hl, 1.0f);
+
+
+//        HorizontalLayout hl2 = new HorizontalLayout();
+//        hl2.setSizeFull();
+//        hl2.setSpacing(true);
+//
+//        tf = new TextField("VSM-Pfad");
+//        tf.setWidth("100%");
+//        Button nt = new NativeButton("...", new ClickListener() {
+//
+//            @Override
+//            public void buttonClick( ClickEvent event )
+//            {
+//                searchDBPath();
+//            }
+//        });
+//        hl2.addComponent(tf);
+//        hl2.addComponent(nt);
+//        hl2.setExpandRatio(tf, 1.0f);
+//
+//        main.addComponent(hl2);
+
+
+        OkAbortPanel okPanel = new OkAbortPanel();
+        okPanel.setOkText(VSMCMain.Txt("Weiter"));
+        
+        addComponent(okPanel);
+        
+        if (!hasAbort)
+            okPanel.getBtAbort().setVisible(false);
+      
+
+
+        final Window w = this;
+        okPanel.getBtAbort().addListener( new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                event.getButton().getApplication().getMainWindow().removeWindow(w);
+            }
+        });
+        okPanel.getBtOk().addListener( new Button.ClickListener() {
+
+            @Override
+            public void buttonClick( ClickEvent event )
+            {
+                if (validator != null && !validator.isValid(getText()))
+                        return;
+
+                event.getButton().getApplication().getMainWindow().removeWindow(w);
+
+                if (okListener != null)
+                {
+                    okListener.buttonClick(event);
+                }
+            }
+        });
+
+        okPanel.getBtOk().focus();
+    }
+
+    public String getText()
+    {
+        return tfText.getValue().toString();
+    }
+
+//    void searchDBPath()
+//    {
+//        //StoragePoolWrapper wr = main.getGuiServerApi().openPoolView(pool, true, pool.getRootDir(), main.getUser());
+//
+//        FSTree tr = DBFSElemEditor.createClientPathTree(main.getGuiServerApi(), wr,tf,  "/", main.getUser(),  DBFSElemEditor.ONLY_DIRS, false, null, null);
+//        editFSTree( tr, wr, main.getGuiServerApi(), tf);
+//
+//        JPAPoolComboField poolCombo = new JPAPoolComboField( main, "poolIdx");
+//
+//
+//        JPADBFSField dbfs = new JPADBFSField(VSMCMain.Txt("Datenbankpfad"), "basePath", main, poolCombo );
+//
+//        poolCombo.createGui(null);
+//        dbfs.createGui(null);
+
+
+//    }
+//
+//    void editFSTree( final FSTree treePanel, final StoragePoolWrapper wrapper, final GuiServerApi api, final TextField tf )
+//    {
+//        if (treePanel == null)
+//            return;
+//
+//        final OkAbortPanel buttonPanel = new OkAbortPanel();
+//
+//
+//        final VerticalLayout vl = new VerticalLayout();
+//        vl.addComponent(treePanel);
+//        vl.setExpandRatio(treePanel, 1.0f);
+//        vl.addComponent(buttonPanel);
+//        vl.setComponentAlignment(buttonPanel, Alignment.BOTTOM_RIGHT);
+//        vl.setSizeFull();
+//        vl.setImmediate(true);
+//        vl.setMargin(true);
+//
+//
+//        final Window win = new Window();
+//        win.setContent(vl);
+//        win.setWidth("650px");
+//        win.setHeight("50%");
+//        win.setCaption(VSMCMain.Txt("Pfadauswahl"));
+//        win.setModal(true);
+//        win.setStyleName("vsm");
+//
+//        this.getApplication().getMainWindow().addWindow(win);
+//
+//        buttonPanel.getBtOk().addListener( new Button.ClickListener()
+//        {
+//
+//            @Override
+//            public void buttonClick( ClickEvent event )
+//            {
+//                Object o = treePanel.getValue();
+//                if (o instanceof RemoteFSElemTreeElem)
+//                {
+//                    RemoteFSElemTreeElem rfs = (RemoteFSElemTreeElem)o;
+//                    tf.setValue(DBFSElemEditor.buildPath(rfs));
+//                }
+//                api.closePoolView(wrapper);
+//
+//
+//                event.getButton().getApplication().getMainWindow().removeWindow(win);
+//            }
+//        });
+//        buttonPanel.getBtAbort().addListener( new Button.ClickListener()
+//        {
+//
+//            @Override
+//            public void buttonClick( ClickEvent event )
+//            {
+//                api.closePoolView(wrapper);
+//                event.getButton().getApplication().getMainWindow().removeWindow(win);
+//            }
+//        });
+//
+//
+//    }
+}
