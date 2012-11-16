ba273b8f64a83c78509629a5c0b8b923a09786d0
Mark Williams <mark@dimm.de>
2012-11-16 18:28:38 +0000

    Allow Links as Dirs

--- src/de/dimm/vsm/vaadin/GuiElems/FileSystem/RemoteFSElemEditor.java
+++ src/de/dimm/vsm/vaadin/GuiElems/FileSystem/RemoteFSElemEditor.java
@@ -204,7 +204,7 @@ public class RemoteFSElemEditor extends LocalFSElemEditor
 
                     if ((options & ONLY_DIRS) != 0)
                     {
-                        if (!rfse.isDirectory())
+                        if (!rfse.isDirectory() && !rfse.isSymbolicLink())
                             continue;
                     }
 
